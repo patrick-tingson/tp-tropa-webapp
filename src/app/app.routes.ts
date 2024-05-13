590bdd1c8796ec6dfe30ab376a4328bc48767a98
patrick-tingson <patrick_tingson@yahoo.com>
2024-05-14 00:03:59 +0800

    Added guard for expired group and invalid group id or nickname

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { JoinGroupComponent } from './features/join-group/join-group.component';
 import { ChatGroupComponent } from './features/chat-group/chat-group.component';
+import { authGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: JoinGroupComponent },
-    {path: 'chat', component: ChatGroupComponent },
+    {path: 'chat', component: ChatGroupComponent, canActivate: [authGuard] },
 ];
--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,5 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
+import moment from 'moment';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+  if(localStorage.getItem(environment.g) 
+    && localStorage.getItem(environment.g)
+    && localStorage.getItem(environment.e))
+  {
+    if (moment().unix() <= moment(localStorage.getItem(environment.e)).unix()) {
+      return true;
+    }
+  }
+  router.navigate(['/']);
+  return false;
 };
